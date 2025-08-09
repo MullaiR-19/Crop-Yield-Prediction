06ae9ee99f4268006563a3a9450146e9f3ac04bd
Mullaivendhan <64030799+MullaiR-19@users.noreply.github.com>
2025-08-09 15:30:02 +0530

    Update Crop_yield_prediction.ino

--- Crop_yield_prediction/Crop_yield_prediction.ino
+++ Crop_yield_prediction/Crop_yield_prediction.ino
@@ -1,9 +1,16 @@
 #include <DHT.h>
+#include <Servo.h>
 
-#define DHTPIN 12       // DHT11 sensor pin
-#define DHTTYPE DHT11   // DHT11 type
+Servo myServo_1;
+Servo myServo_2;
+
+#define DHTPIN 12      // DHT11 sensor pin
+#define DHTTYPE DHT11  // DHT11 type
 #define LED_PIN 2      // LED pin 13
 #define RELAY_PIN 6    // Relay Pin 6
+#define SERVO_PIN_1    // Main Servo 3
+#define SERVO_PIN_2    //Sprinkler Servo 5
+
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -12,13 +19,18 @@ void setup() {
   dht.begin();
   pinMode(LED_PIN, OUTPUT);
   pinMode(RELAY_PIN, OUTPUT);
+  myServo_1.attach(SERVO_PIN_1);
+  myServo_2.attach(SERVO_PIN_2);
+
+  myServo_1.write(90);
+  myServo_2.write(30);
 }
 
 void loop() {
-  int soil = map(analogRead(A0), 0, 1023, 0, 100);    // Soil moisture %
-  float temp = dht.readTemperature();                 // Temperature (°C)
-  float hum = dht.readHumidity();                     // Humidity (%)
-  int lux = analogRead(A5);                           // Light intensity (raw)
+  int soil = map(analogRead(A0), 0, 1023, 0, 100);  // Soil moisture %
+  float temp = dht.readTemperature();               // Temperature (°C)
+  float hum = dht.readHumidity();                   // Humidity (%)
+  int lux = analogRead(A5);                         // Light intensity (raw)
 
   // Check if DHT reading failed
   if (isnan(temp) || isnan(hum)) {
@@ -45,9 +57,43 @@ void loop() {
   if (Serial.available()) {
     char cmd = Serial.read();
     if (cmd == 'W') {
-      digitalWrite(RELAY_PIN, HIGH);
-      delay(10000); // 10 seconds
+      myServo_1.write(90);
+
+      sprinkle();
+      for (int i = 90; i >= 10; i--) {
+        myServo_1.write(i);
+        delay(15);
+      }
+
+      sprinkle();
+      for (int i = 10; i <= 170; i++) {
+        myServo_1.write(i);
+        delay(15);
+      }
+
+      sprinkle();
+      for (int i = 170; i >= 90; i--) {
+        myServo_1.write(i);
+        delay(15);
+      }
+
+      delay(1000);
       digitalWrite(RELAY_PIN, LOW);
     }
   }
+}
+
+//Sprinkler head rotate function
+void sprinkle() {
+  digitalWrite(RELAY_PIN, HIGH);
+  for (i = 30; i <= 150; i++) {
+    myServo_2.write(i);
+    dela(40);
+  }
+  for (i = 150; i < = 30; i--) {
+    myServo_2.write(i);
+    dela(40);
+  }
+  digitalWrite(RELAY_PIN, LOW);
+  delay(200);
 }
